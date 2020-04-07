4c91c3a18b5339f2bbfdd1c5c127940208f73037
Luis Hernandez <lhdez@iuma.ulpgc.es>
2020-04-07 08:33:59 +0100

    cambios menores

--- app/src/androidTest/java/es/ulpgc/eite/cleancode/calculator/BasicActivityInstrumentedTests.java
+++ app/src/androidTest/java/es/ulpgc/eite/cleancode/calculator/BasicActivityInstrumentedTests.java
@@ -1,25 +1,24 @@
 package es.ulpgc.eite.cleancode.calculator;
 
 
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import es.ulpgc.eite.cleancode.calculator.R;
 import es.ulpgc.eite.cleancode.calculator.basic.BasicActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
